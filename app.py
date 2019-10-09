0c971aea508c5d8b91ec8c980ed8196a87401414
omarsagoo <om.sagoo@gmail.com>
2019-10-08 22:35:01 -0700

    create HTML elements for webpage, finish most CRUD routes

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template("base.html")
+    return render_template("dreams_index.html", dreams=dreams)
 
 @app.route('/dreams/new')
 def dreams_new():
@@ -23,7 +23,7 @@ def dreams_new():
     return render_template('dream_new.html', dream={}, title='New Dream')
 
 @app.route('/dreams', methods=['POST'])
-def playlists_submit():
+def dreams_submit():
     """Submit a new dream."""
     dream = {
         'title': request.form.get('title'),
@@ -32,6 +32,8 @@ def playlists_submit():
         # 'created_at': datetime.now()
     }
     dream_id = dreams.insert_one(dream).inserted_id
+    print(request.form.to_dict())
+
     return redirect(url_for('dreams_show', dream_id=dream_id))
 
 @app.route('/dreams/<dream_id>')
@@ -42,7 +44,7 @@ def dreams_show(dream_id):
     return render_template('dreams_show.html', dream=dream)
 
 @app.route('/dreams/<dream_id>/edit')
-def playlists_edit(dream_id):
+def dreams_edit(dream_id):
     """Show the edit form for a dream."""
     dream = dreams.find_one({'_id': ObjectId(dream_id)})
     return render_template('dreams_edit.html', dream=dream, title='Edit Dream')
@@ -64,7 +66,7 @@ def dreams_update(dream_id):
 def dreams_delete(dream_id):
     """Delete one dream."""
     dreams.delete_one({'_id': ObjectId(dream_id)})
-    return redirect(url_for('dreams_index'))
+    return redirect(url_for('dreams_new'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
