294acc35f125ce74b3455480266208a3bcd9d665
omarsagoo <om.sagoo@gmail.com>
2019-10-11 13:52:42 -0700

    fix bug

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ from pymongo import MongoClient
 from datetime import datetime
 
 host = os.environ.get('MONGODB_URI', 'mongodb://omarsagoo:makeschool2019@ds233268.mlab.com:33268/heroku_vjnrq1bv')
-client = MongoClient(host=f'{host}?retryWrites=false')
-db = client.get_default_database()
+client = MongoClient(host=f'{host}?retryWrites=false',
+                     username='omarsagoo', 
+                     password='mmakeschool2019',
+                     authSource="admin")
+db = client['Dreams']
+# client = MongoClient(host=f'{host}?retryWrites=false')
+# db = client.get_default_database()
 dreams = db.dreams
 users = db.users
 comments = db.comments
